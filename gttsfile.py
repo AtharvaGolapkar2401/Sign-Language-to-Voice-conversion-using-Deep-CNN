68067928409a6b988d25f71fb8f4b12428f7fa81
AtharvaGolapkar2401 <atharvagolapkar@gmail.com>
2023-04-10 13:50:03 +0530

    newly added files

--- gttsfile.py
+++ gttsfile.py
@@ -3,7 +3,7 @@ from playsound import playsound
 
 def convert_to_audio(text):
     my_audio = gTTS(text)
-    my_audio.save('repeat.mp3')
+    my_audio.save('ghatkopar.mp3')
 
-convert_to_audio("कृपया इसे एक बार फिर से दोहराएं ?")
+convert_to_audio("via Kurla or Thane")
 # playsound('single.mp3', 'return.mp3')
--- splittingdb.py
+++ splittingdb.py
@@ -2,6 +2,6 @@ import splitfolders
 
 input_folder = 'data2/'
 
-splitfolders.ratio(input_folder, output='dataset', seed=42, ratio=(.7, .2, .1), group_prefix=None)
+splitfolders.ratio(input_folder, output='dataset2', seed=42, ratio=(.7, .2, .1), group_prefix=None)
 
-splitfolders.fixed(input_folder, output='dataset', seed=42, fixed=(200, 200), oversample=False, group_prefix=None)
+splitfolders.fixed(input_folder, output='dataset2', seed=42, fixed=(200, 200), oversample=False, group_prefix=None)
